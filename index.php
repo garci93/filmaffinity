b571f82206c3bd059c779ab72face5aedd7464b8
José Joaquín Garci <josejoaquin.garci@iesdonana.org>
2018-10-26 21:17:52 +0200

    Alternativa con while

--- index.php
+++ index.php
@@ -8,15 +8,20 @@
         <?php
         $pdo = new PDO('pgsql:host=localhost;dbname=fa', 'fa', 'fa');
         $st = $pdo->query('SELECT * FROM generos');
-        $res = $st->fetchAll();
         ?>
-        <table border="1">
+        <table border="1" style="margin:auto">
             <thead>
                 <th>Id</th>
                 <th>Género</th>
             </thead>
             <tbody>
-                <?php foreach ($res as $fila): ?>
+                <!-- <?php while ($fila = $st->fetch()): ?>
+                    <tr>
+                        <td><?= $fila['id'] ?></td>
+                        <td><?= $fila['genero'] ?></td>
+                    </tr>
+                <?php endwhile ?> -->
+                <?php foreach ($st as $fila): ?>
                     <tr>
                         <td><?= $fila['id'] ?></td>
                         <td><?= $fila['genero'] ?></td>
