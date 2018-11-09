76ad8fefa67ff6fbcce821a3200518e2872ac2b4
José Joaquín Garci <josejoaquin.garci@iesdonana.org>
2018-11-09 21:23:40 +0100

    Comienzo del login

--- comunes/auxiliar.php
+++ comunes/auxiliar.php
@@ -230,3 +230,35 @@ function selected($a, $b)
 {
     return $a == $b ? 'selected' : '';
 }
+
+function comprobarLogin(&$error)
+{
+    $login = trim(filter_input(INPUT_POST,'login'));
+    if ($login === ''){
+        $error['login'] = 'El nombre de usuario no puede estar vacío.';
+    }
+}
+
+function comprobarPassword(&$error)
+{
+    $password = trim(filter_input(INPUT_POST,'password'));
+    if (password === ''){
+        $error['password'] = 'La contraseña no puede estar vacía.';
+    }
+}
+
+function comprobarPassword($valores, $pdo, &$error)
+{
+    extract($valores);
+    $st = $pdo->prepare('SELECT *
+                           FROM usuarios
+                          WHERE login = :login');
+    $st->execute(['login' => $login]);
+    $fila = $st->fetch();
+    if ($fila !== false) {
+        if (password_verify($password, $fila['password'])) {
+            return;
+        }
+    }
+    $error['sesion'] = 'El usuario o la contraseña son incorrectos.';
+}
